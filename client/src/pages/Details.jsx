851a5a245f4e7e6553e5db6ccb4ddc5996673721
Baptiste Frere <baptiste.frere@etu.unice.fr>
2020-02-10 14:16:19 +0100

    details is now a functional component

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -32,99 +32,90 @@ function TagItem(props) {
 function youTube(videoId) {
     if(videoId!==""){
         return(
-            <div className="youTube">
+            <div className="detailYoutube">
                 <YouTube videoId={videoId}/>
             </div>
         )
     }
 }
 
-class Details extends Component{
-    constructor(props) {
-        super(props);
+function addToCard(name){
+    toast.success("Plugin "+name+" added to cart.");
+}
 
-        let testplugin = {
-            name: "bASS",
-            version: "1.0.1",
-            description: "The best bass you will every find... and this should be longer because it is a description lol do you like this text? Such beautiful text much words. Is this enough? Do you think we need more text? We need more ziggurats. Ok I stop now",
-            author: "FluidGM",
-            image: "bASS.png",
-            //next are optional
-            //codeLink: String,
-            categories: ["MODULATION","REVERB"],
-            tags: ["tag1","tag2","tag3"],
-            youtubeLink: "B3WJaC-7g2c",
-            //likes: [String], // user mails
-            //comments: [{type: mongoose.Schema.ObjectId, ref: 'comments'}],
-            /*status: {
-                available: {type: Boolean, default: false},
-                automaticValidation: {type: Boolean, default: false},
-                manualValidation: {type: Boolean, default: false},
-            },*/
-            tryLink: "https://www.google.be/",
-            price: 15.99,
-            zipLocation: "not sure what is supposed to be here",
-        };
+function Details(){
 
-        this.state = {
-            plugin : testplugin
-        };
-    }
+    let testplugin = {
+        name: "bASS",
+        version: "1.0.1",
+        description: "The best bass you will every find... and this should be longer because it is a description lol do you like this text? Such beautiful text much words. Is this enough? Do you think we need more text? We need more ziggurats. Ok I stop now",
+        author: "FluidGM",
+        image: "bASS.png",
+        //next are optional
+        //codeLink: String,
+        categories: ["MODULATION","REVERB"],
+        tags: ["tag1","tag2","tag3"],
+        youtubeLink: "B3WJaC-7g2c",
+        //likes: [String], // user mails
+        //comments: [{type: mongoose.Schema.ObjectId, ref: 'comments'}],
+        /*status: {
+            available: {type: Boolean, default: false},
+            automaticValidation: {type: Boolean, default: false},
+            manualValidation: {type: Boolean, default: false},
+        },*/
+        tryLink: "https://www.google.be/",
+        price: 15.99,
+        zipLocation: "not sure what is supposed to be here",
+    };
 
-    componentWillMount() {
-        //TODO:replace testplugin with the real plugin from DB
-    }
+    let plugin = testplugin;
 
-    addToCard = () => toast.success("Plugin "+this.state.plugin.name+" added to cart.");
-
-    render() {
-        return (
-            <Card className="card" variant="outlined">
-                <div className="wrapper">
-                    <CardContent className="leftCard">
-                        <CardMedia className="media">
-                            <img src={require("../assets/images/"+this.state.plugin.image)}/>
-                        </CardMedia>
-                    </CardContent>
-                    <CardContent className="rightCard">
-                        <Typography color="textSecondary" gutterBottom style={{float: "left"}}>
-                            Category :
-                        </Typography>
-                        {this.state.plugin.categories.map((item, index) => (
-                            <CategoryItem key={index} item={item} />
-                        ))}
-                        <Typography variant="h5" component="h2">
-                            {this.state.plugin.name}
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            {"v"+this.state.plugin.version}
-                        </Typography>
-                        <Typography className="title" color="textSecondary" gutterBottom>
-                            {this.state.plugin.price+" €"}
-                        </Typography>
-                        <Button variant="contained" onClick={() => this.addToCard()}>Add to cart</Button>
-                        <div style={{marginTop: "10px"}}>
-                            {this.state.plugin.tags.map((item, index) => (
-                                <TagItem key={index} item={item} />
-                            ))}
-                        </div>
-                    </CardContent>
-                </div>
-                <CardContent>
-                    <Typography className="title" color="textSecondary" gutterBottom>
-                        Description
+    return (
+        <Card className="detailCard" variant="outlined">
+            <div className="detailWrapper">
+                <CardContent className="leftCard">
+                    <CardMedia className="detailMedia">
+                        <img src={require("../assets/images/"+plugin.image)}/>
+                    </CardMedia>
+                </CardContent>
+                <CardContent className="rightCard">
+                    <Typography color="textSecondary" gutterBottom style={{float: "left"}}>
+                        Category :
+                    </Typography>
+                    {plugin.categories.map((item, index) => (
+                        <CategoryItem key={index} item={item} />))
+                    }
+                    <Typography variant="h5" component="h2">
+                        {plugin.name}
                     </Typography>
                     <Typography variant="body2" component="p">
-                        {this.state.plugin.description}
+                        {"v"+plugin.version}
                     </Typography>
+                    <Typography className="detailTitle" color="textSecondary" gutterBottom>
+                        {plugin.price+" €"}
+                    </Typography>
+                    <Button variant="contained" onClick={() => addToCard(plugin.name)}>Add to cart</Button>
+                    <div style={{marginTop: "10px"}}>
+                        {plugin.tags.map((item, index) => (
+                            <TagItem key={index} item={item} />))
+                        }
+                    </div>
                 </CardContent>
-                {youTube(this.state.plugin.youtubeLink)}
-                <CardActions>
-                    <Button size="small" href={this.state.plugin.tryLink}>Try now</Button>
-                </CardActions>
-            </Card>
-        );
-    }
+            </div>
+            <CardContent>
+                <Typography className="detailTitle" color="textSecondary" gutterBottom>
+                    Description
+                </Typography>
+                <Typography variant="body2" component="p">
+                    {plugin.description}
+                </Typography>
+            </CardContent>
+            {youTube(plugin.youtubeLink)}
+            <CardActions>
+                <Button size="small" href={plugin.tryLink}>Try now</Button>
+            </CardActions>
+        </Card>
+    );
 }
 
 export default Details;
