08bf48fd7921903301376b40f0677fda1eb23002
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-02-17 16:29:30 +0100

    Added baseURL to api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,24 +1,17 @@
 import axios from 'axios'
 
+export const baseURL = 'http://localhost:3000/';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: baseURL,
 });
+
 export const createPlugin = (payload, headers) => api.post('/plugins', payload, headers);
 export const createAnAccount = payload => api.post('users', payload);
 export const connectToAccount = payload => api.post('users/login', payload);
-export const insertMovie = payload => api.post(`/movie`, payload);
-export const getAllMovies = () => api.get(`/movies`);
-export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload);
-export const deleteMovieById = id => api.delete(`/movie/${id}`);
-export const getMovieById = id => api.get(`/movie/${id}`);
 
 const apis = {
     createPlugin,
-    insertMovie,
-    getAllMovies,
-    updateMovieById,
-    deleteMovieById,
-    getMovieById,
     createAnAccount,
     connectToAccount
 };
