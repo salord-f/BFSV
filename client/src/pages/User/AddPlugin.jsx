ea5c5a7d19530dc8b73691597e43039a78cf616c
Baptiste Frere <baptiste.frere@etu.unice.fr>
2020-02-24 14:55:45 +0100

    Check for obligatory fields

--- client/src/pages/User/AddPlugin.jsx
+++ client/src/pages/User/AddPlugin.jsx
@@ -5,6 +5,7 @@ import api from './../../api'
 import Typography from "@material-ui/core/Typography";
 import { useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom';
+import {toast} from 'react-toastify';
 
 
 export default function AddPlugin(props) {
@@ -31,22 +32,41 @@ export default function AddPlugin(props) {
     }, [i, login])
 
     const handleSubmit = async (event) => {
-        event.preventDefault();
-        const formData = new FormData();
-        formData.append('name', name);
-        formData.append('version', version);
-        formData.append('description', description);
-        formData.append('image', image);
-        formData.append('plugin', plugin);
-        formData.append('author', login.user.mail);
-        await api.createPlugin(formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'Authorization': 'Bearer ' + login.user.token
-            }
-        }, (res) => {
-            console.log(res)
-        });
+        const regex = RegExp('\\d.\\d.\\d');
+        if(!name || !description || !version || !image || !plugin){
+            toast.error(<div>
+                <p>Veuillez remplir les champs suivants</p>
+                <ul>
+                    {!name && <li>Nom</li>}
+                    {!version && <li>Version</li>}
+                    {!description && <li>Description</li>}
+                    {!image && <li>Image</li>}
+                    {!plugin && <li>Code du plugin</li>}
+                </ul>
+                </div>)
+        } else if(!regex.test(version)) {
+            toast.error("Veuillez entrer un numéro de version valide.")
+        } else if(plugin.name.split('.').pop()!=="zip") {
+            toast.error("Le plugin doit etre au format .zip")
+        } else {
+            toast.success("Votre plugin a bien été ajouté.")
+            event.preventDefault();
+            const formData = new FormData();
+            formData.append('name', name);
+            formData.append('version', version);
+            formData.append('description', description);
+            formData.append('image', image);
+            formData.append('plugin', plugin);
+            formData.append('author', login.user.mail);
+            await api.createPlugin(formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': 'Bearer ' + login.user.token
+                }
+            }, (res) => {
+                console.log(res)
+            });
+        }
     };
 
     const handleNameChange = (event) => {
@@ -84,15 +104,15 @@ export default function AddPlugin(props) {
                             style={{ "width": "100%", "marginTop": "10px" }}>Ajouter un plugin</Typography>
                     </Grid>
                     <Grid item>
-                        <TextField label={'Nom'} onChange={handleNameChange} required
+                        <TextField error={!name} label={'Nom'} onChange={handleNameChange} required
                             style={{ "width": "100%", "marginTop": "10px" }}>Nom</TextField>
                     </Grid>
                     <Grid item>
-                        <TextField label={'Version'} onChange={handleVersionChange} required
+                        <TextField error={!version} label={'Version'} onChange={handleVersionChange} required
                             style={{ "width": "100%", "marginTop": "10px" }}>Version</TextField>
                     </Grid>
                     <Grid item>
-                        <TextField label={'Description'} onChange={handleDescriptionChange} required
+                        <TextField error={!description} label={'Description'} onChange={handleDescriptionChange} required
                             style={{ "width": "100%", "marginTop": "10px" }}>Description</TextField>
 
                     </Grid>
